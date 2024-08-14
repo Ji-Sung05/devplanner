c4e280d6c1d30b7ad1181e8cac4ade31930c8622
Ji-Sung05 <dlwltjd6847@gmail.com>
2024-08-14 15:54:40 +0900

    Fix: "board 페이지 스타일 변경"
    boardSection flex-row로 변경

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,13 +3,15 @@ import Header from '../components/layout/Header'
 import Aside from '../components/layout/Aside'
 import Main from '../components/layout/Main'
 import Landing from './Landing'
+import { useLocation } from 'react-router-dom'
 
 const Home = (props) => {
-  
+  const location = useLocation()
+  const isWorkPage = location.pathname.slice(0,5) === '/work';
   return (
     <div id='container'>
       <Header />
-        <div className={`container__inner`}>
+        <div className={`container__inner ${isWorkPage ? 'work' : ''}`}>
           <Aside />
           <Main>
             {props.children ? props.children : <Landing />}
