c37877a164089fa8a29cac15e549dc59d81c7ade
Ji-Sung05 <dlwltjd6847@gmail.com>
2024-08-18 22:16:12 +0900

    Fix: "logout 코드 제거"

--- src/components/AuthContext.js
+++ src/components/AuthContext.js
@@ -1,5 +1,5 @@
 import React, { createContext, useState, useEffect } from 'react';
-import { useGetAuthQuery, useLogoutMutation } from '../app/usersSlice';
+import { useGetAuthQuery } from '../app/usersSlice';
 import { getCookie, removeCookie } from '../cookies/Cookies';
 
 export const AuthContext = createContext();
@@ -7,7 +7,6 @@ export const AuthContext = createContext();
 export const AuthProvider = ({ children }) => {
   const { isSuccess } = useGetAuthQuery();
   const [isAuth, setIsAuth] = useState(isSuccess);
-  const [logout] = useLogoutMutation();
   let token = getCookie('token');
 
   useEffect(() => {
@@ -18,22 +17,13 @@ export const AuthProvider = ({ children }) => {
     }
   }, [token, isSuccess]);
 
-  const handleLogout = async () => {
-    try {
-      await logout().unwrap();
-      removeCookie('token')
-      setIsAuth(false)
-      window.location.href = '/login'; // 로그아웃 후 로그인 페이지로 리디렉트
-    } catch (error) {
-      console.error('Logout failed:', error);
-      removeCookie('token');
-      setIsAuth(false);
-      window.location.href = '/login';
-    }
+  const logout = () => {
+    removeCookie('token');
+    setIsAuth(false);
   };
 
   return (
-    <AuthContext.Provider value={{ isAuth, handleLogout }}>
+    <AuthContext.Provider value={{ isAuth, logout }}>
       {children}
     </AuthContext.Provider>
   );
--- src/components/UI/LogoutBtn.jsx
+++ src/components/UI/LogoutBtn.jsx
@@ -2,10 +2,10 @@ import React, { useContext } from 'react'
 import { AuthContext } from '../AuthContext'
 
 const LogoutBtn = () => {
-  const { handleLogout } = useContext(AuthContext);
+  const { logout } = useContext(AuthContext);
 
   return (
-    <div className='logoutBtn' onClick={handleLogout}>Logout</div>
+    <div className='logoutBtn' onClick={logout}>Logout</div>
   )
 }
 
