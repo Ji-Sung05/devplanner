d1f7315d76f935fadf2023d6c995b19334596b80
Ji-Sung05 <dlwltjd6847@gmail.com>
2024-08-27 22:32:09 +0900

    Fix: "로그아웃 코드 수정"

--- src/components/AuthContext.js
+++ src/components/AuthContext.js
@@ -20,9 +20,9 @@ export const AuthProvider = ({ children }) => {
 
   const handleLogout = async () => {
     try {
-      await logout().unwrap();
+      //await logout().unwrap();
       setIsAuth(false);
-      window.location.href = "https://accounts.google.com/Logout";
+      window.location.href = "/";
     } catch (err) {
       console.error("Logout failed:", err);
     }
