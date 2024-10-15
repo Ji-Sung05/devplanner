adfa2e56800b6dd2bbcb255db1f100de3bf6afdf
Ji-Sung05 <dlwltjd6847@gmail.com>
2024-10-15 21:39:19 +0900

    Feat: "HashRouter 적용"

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './assets/scss/style.scss';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 import { CookiesProvider } from 'react-cookie';
 import { Provider } from 'react-redux';
 import store from './app/store';
@@ -11,9 +11,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
     <CookiesProvider>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </CookiesProvider>
   </Provider>
 );
