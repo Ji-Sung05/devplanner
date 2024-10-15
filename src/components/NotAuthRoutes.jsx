17eef38a5686262e75cebe3864180fc1a4aa9e74
Ji-Sung05 <dlwltjd6847@gmail.com>
2024-10-15 22:19:54 +0900

    Feat: "페이지 설정"

--- src/components/NotAuthRoutes.jsx
+++ src/components/NotAuthRoutes.jsx
@@ -1,15 +1,18 @@
-import React, { useContext } from 'react'
-import { Navigate, Outlet, useLocation } from 'react-router-dom'
+import React, { useContext } from 'react';
+import { Navigate, Outlet, useLocation } from 'react-router-dom';
 import { AuthContext } from './AuthContext';
 
 const NotAuthRoutes = () => {
   const { isAuth } = useContext(AuthContext);
   const location = useLocation();
-  console.log(location)
 
-  return (
-    isAuth && location.pathname === '/' ? <Navigate to="/home" /> : <Outlet />
-  )
-}
+  // 사용자가 인증되어 있고, 현재 경로가 루트('/')일 때만 '/home'으로 리다이렉트
+  if (isAuth && location.pathname === '/') {
+    return <Navigate to="/home" />;
+  }
 
-export default NotAuthRoutes
+  // 인증되지 않았을 경우 현재 페이지를 유지
+  return <Outlet />;
+};
+
+export default NotAuthRoutes;
