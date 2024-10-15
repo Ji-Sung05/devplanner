6eb7501e127343e7eb941a15b336e1aa49fff9a0
Ji-Sung05 <dlwltjd6847@gmail.com>
2024-10-15 21:54:23 +0900

    Feat: "loading 설정"

--- src/components/NotAuthRoutes.jsx
+++ src/components/NotAuthRoutes.jsx
@@ -3,7 +3,11 @@ import { Navigate, Outlet } from 'react-router-dom'
 import { AuthContext } from './AuthContext';
 
 const NotAuthRoutes = () => {
-  const { isAuth } = useContext(AuthContext);
+  const { isAuth, loading } = useContext(AuthContext);
+
+  if (loading) {
+    return <div>Loading...</div>;  // 로딩 중일 때 리다이렉션 하지 않음
+  }
   return (
     isAuth ? <Navigate to={'/home'} /> : <Outlet />
   )
--- src/components/ProtectedRoutes.jsx
+++ src/components/ProtectedRoutes.jsx
@@ -3,7 +3,11 @@ import { Navigate, Outlet } from 'react-router-dom'
 import { AuthContext } from './AuthContext';
 
 const ProtectedRoutes = () => {
-  const { isAuth } = useContext(AuthContext);
+  const { isAuth, loading } = useContext(AuthContext);
+
+  if(loading) {
+    return <div>Loading...</div>
+  }
   return (
     isAuth ? <Outlet /> : <Navigate to={'/'} />
   )
